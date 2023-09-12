94c5da2b9ec9849ff9b2cc3be1a2ad4ab9d44d64
JRaghu842 <jraghavendra842@gmail.com>
2023-09-13 00:11:57 +0530

    collapsable accordian created

--- vid5_food_app/src/components/About.js
+++ vid5_food_app/src/components/About.js
@@ -1,38 +1,6 @@
-// import { Link, Outlet } from "react-router-dom";
-// import CareerClassCompo from "./CareerClass";
-// import React, { useEffect } from "react";
-
-import React, { useState } from "react";
-
-let Section = ({ title, description }) => {
-  let [isVisible, setIsVisible] = useState(false);
-  return (
-    <div className="border-2 border-black rounded-lg p-2 m-2">
-      <h1 className="font-bold text-xl">{title}</h1>
-      {isVisible === true ? (
-        <button
-          className="border-2 border-black p-2 rounded-lg"
-          onClick={() => {
-            setIsVisible(false);
-          }}
-        >
-          Hide
-        </button>
-      ) : (
-        <button
-          className="border-2 border-black p-2 rounded-lg"
-          onClick={() => {
-            setIsVisible(true);
-          }}
-        >
-          Show
-        </button>
-      )}
-
-      {isVisible && <p>{description}</p>}
-    </div>
-  );
-};
+import { Link, Outlet } from "react-router-dom";
+import CareerClassCompo from "./CareerClass";
+import React, { useEffect } from "react";
 
 // Functional Component ABOUT
 
@@ -106,142 +74,88 @@ let Section = ({ title, description }) => {
 
 // Class based component for ABOUT section
 
-// class About extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     console.log("Parent-Constructor");
-//   }
+class About extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log("Parent-Constructor");
+  }
 
-//   componentDidMount() {
-//     // API call here
-//     console.log("Parent-componentDidMount");
-//   }
+  componentDidMount() {
+    // API call here
+    console.log("Parent-componentDidMount");
+  }
 
-//   componentDidUpdate() {
-//     console.log("Parent-componentDidUpdate");
-//   }
+  componentDidUpdate() {
+    console.log("Parent-componentDidUpdate");
+  }
 
-//   componentWillUnmount() {
-//     console.log("Parent-componentWillUnmount");
-//   }
+  componentWillUnmount() {
+    console.log("Parent-componentWillUnmount");
+  }
 
-//   render() {
-//     console.log("Parent-render");
-//     return (
-//       <div className="About_container">
-//         <section className="abt-section-1">
-//           <h1>This is About page for Foodvilla company pvt LTD.</h1>
-//           <p>Hello! wellcome to the world of food, Just EAT and repEAT 😋</p>
-//         </section>
-//         <section>
-//           <h2>Our Mission</h2>
-//           <p>
-//             Our mission is to provide high-quality products/services that meet
-//             the needs of our customers while maintaining a commitment to
-//             sustainability and social responsibility.
-//           </p>
-//         </section>
-//         <section className="abt-section-2">
-//           <h2>Our Values</h2>
-//           <ul>
-//             <li>Customer Satisfaction</li>
-//             <li>Integrity and Transparency</li>
-//             <li>Innovation</li>
-//             <li>Community Engagement</li>
-//           </ul>
-//         </section>
-//         <h2 className="abt-h2">Meet Our Team</h2>
-//         <div className="abt-team-div">
-//           <div className="team-member">
-//             <img
-//               src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080194-7382724028-unkno.png"
-//               alt="Roronoro ZORO - CEO"
-//             />
-//             <h3>Roronoro Zoro</h3>
-//             <p>CEO</p>
-//           </div>
-//           <div className="team-member">
-//             <img
-//               src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7078671-9363995852-39fc3.jpg"
-//               alt="ACE - CTO"
-//             />
-//             <h3> Ace D Roger</h3>
-//             <p>CTO</p>
-//           </div>
-//           <div className="team-member">
-//             <img
-//               src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080202-1635462455-unkno.png"
-//               alt="LUFFy - CFO"
-//             />
-//             <h3> Monkey D Loffy</h3>
-//             <p>CFO</p>
-//           </div>
-//         </div>
-//         <div className="career">
-//           <CareerClassCompo name={"Class based Props"} />
-//         </div>
-//         <Link to={"/about/profile"}>
-//           <h3 className="abt_auth">CLICK HERE TO GET MORE ABOUT AUTHOR</h3>
-//         </Link>
-//         <Outlet />
-//       </div>
-//     );
-//   }
-// }
-
-let About = () => {
-  return (
-    <div>
-      <Section
-        title={"About us"}
-        description={
-          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,"
-        }
-      />
-
-      <Section
-        title={"Our Mission"}
-        description={
-          "Our mission is to provide high-quality products/services that meet the needs of our customers while maintaining a commitment tosustainability and social responsibility."
-        }
-      />
-
-      <Section
-        title={"Contact us"}
-        description={
-          "a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC."
-        }
-      />
-
-      <div className="abt-team-div">
-        <div className="team-member">
-          <img
-            src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080194-7382724028-unkno.png"
-            alt="Roronoro ZORO - CEO"
-          />
-          <h3>Roronoro Zoro</h3>
-          <p>CEO</p>
+  render() {
+    console.log("Parent-render");
+    return (
+      <div className="About_container">
+        <section className="abt-section-1">
+          <h1>This is About page for Foodvilla company pvt LTD.</h1>
+          <p>Hello! wellcome to the world of food, Just EAT and repEAT 😋</p>
+        </section>
+        <section>
+          <h2>Our Mission</h2>
+          <p>
+            Our mission is to provide high-quality products/services that meet
+            the needs of our customers while maintaining a commitment to
+            sustainability and social responsibility.
+          </p>
+        </section>
+        <section className="abt-section-2">
+          <h2>Our Values</h2>
+          <ul>
+            <li>Customer Satisfaction</li>
+            <li>Integrity and Transparency</li>
+            <li>Innovation</li>
+            <li>Community Engagement</li>
+          </ul>
+        </section>
+        <h2 className="abt-h2">Meet Our Team</h2>
+        <div className="abt-team-div">
+          <div className="team-member">
+            <img
+              src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080194-7382724028-unkno.png"
+              alt="Roronoro ZORO - CEO"
+            />
+            <h3>Roronoro Zoro</h3>
+            <p>CEO</p>
+          </div>
+          <div className="team-member">
+            <img
+              src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7078671-9363995852-39fc3.jpg"
+              alt="ACE - CTO"
+            />
+            <h3> Ace D Roger</h3>
+            <p>CTO</p>
+          </div>
+          <div className="team-member">
+            <img
+              src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080202-1635462455-unkno.png"
+              alt="LUFFy - CFO"
+            />
+            <h3> Monkey D Loffy</h3>
+            <p>CFO</p>
+          </div>
         </div>
-        <div className="team-member">
-          <img
-            src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7078671-9363995852-39fc3.jpg"
-            alt="ACE - CTO"
-          />
-          <h3> Ace D Roger</h3>
-          <p>CTO</p>
+        <div className="career">
+          <CareerClassCompo name={"Class based Props"} />
         </div>
-        <div className="team-member">
-          <img
-            src="https://comicvine.gamespot.com/a/uploads/square_small/11117/111178336/7080202-1635462455-unkno.png"
-            alt="LUFFy - CFO"
-          />
-          <h3> Monkey D Loffy</h3>
-          <p>CFO</p>
-        </div>
+        <Link to={"/about/profile"}>
+          <h3 className="abt_auth">CLICK HERE TO GET MORE ABOUT AUTHOR</h3>
+        </Link>
+        <Outlet />
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 // React life cycle ORDER is given below
 
--- vid5_food_app/src/components/Instamart.js
+++ vid5_food_app/src/components/Instamart.js
@@ -1,10 +1,95 @@
-const Instamart = () => {
+import React, { useState } from "react";
+
+// Short cut for toggle
+// let Section = ({ title, description, isVisible, setIsVisible }) => {
+//   return (
+//     <div className="border-2 border-black rounded-lg p-2 m-2">
+//       <h1 className="font-bold text-xl">{title}</h1>
+//       <button
+//         className="border-2 border-black p-2 rounded-lg"
+//         onClick={() => {
+//           setIsVisible(!isVisible);
+//         }}
+//       >
+//         {isVisible ? "Hide" : "Show"}
+//       </button>
+
+//       {isVisible && <p>{description}</p>}
+//     </div>
+//   );
+// };
+
+let Section = ({ title, description, isVisible, setIsVisible }) => {
+  return (
+    <div className="border-2 border-black rounded-lg p-2 m-2">
+      <h1 className="font-bold text-xl">{title}</h1>
+      {isVisible === true ? (
+        <button
+          className="border-2 border-black p-2 rounded-lg"
+          onClick={() => {
+            setIsVisible();
+          }}
+        >
+          Hide
+        </button>
+      ) : (
+        <button
+          className="border-2 border-black p-2 rounded-lg"
+          onClick={() => {
+            setIsVisible();
+          }}
+        >
+          Show
+        </button>
+      )}
+
+      {isVisible && <p>{description}</p>}
+    </div>
+  );
+};
+
+let Instamart = () => {
+  let [visibleSection, setVisibleSection] = useState("about");
   return (
     <div>
-      <h1>INSTAMART</h1>
-      <h2>This is created to understand Chunking/code-spliting</h2>
-      <h3>Assume Instamart is using 100's of components</h3>
-      <h3>Assume Instamart is a large code</h3>
+      <Section
+        title={"About Instamart"}
+        description={
+          "Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum,"
+        }
+        isVisible={visibleSection === "about"}
+        setIsVisible={() => {
+          setVisibleSection((prevState) => {
+            return prevState === "about" ? null : "about";
+          });
+        }}
+      />
+
+      <Section
+        title={"Instamart Mission"}
+        description={
+          "Our mission is to provide high-quality products/services that meet the needs of our customers while maintaining a commitment tosustainability and social responsibility."
+        }
+        isVisible={visibleSection === "mission"}
+        setIsVisible={() => {
+          setVisibleSection((prevState) => {
+            return prevState === "mission" ? null : "mission";
+          });
+        }}
+      />
+
+      <Section
+        title={"Contact Instamart"}
+        description={
+          "a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of  (The Extremes of Good and Evil) by Cicero, written in 45 BC."
+        }
+        isVisible={visibleSection === "contact"}
+        setIsVisible={() => {
+          setVisibleSection((prevState) => {
+            return prevState === "contact" ? null : "contact";
+          });
+        }}
+      />
     </div>
   );
 };
